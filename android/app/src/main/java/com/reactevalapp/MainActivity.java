a4d8c5a1a89ed22442a946ea7dc2a8fed43e2ff0
Jickson P <jickson.p@peppertap.com>
2016-05-15 19:45:14 +0530

    integrated react-native-material-kit

--- android/app/src/main/java/com/reactevalapp/MainActivity.java
+++ android/app/src/main/java/com/reactevalapp/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
 import com.eguma.barcodescanner.BarcodeScanner;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,7 +38,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new BarcodeScanner()
+            new BarcodeScanner(),
+            new ReactMaterialKitPackage() 
         );
     }
 }
