019404e9235c98cb9fb91fcfe3141f641b2c3938
Jickson P <jickson.p@peppertap.com>
2016-05-19 17:47:33 +0530

    Show github user details

--- src/scenes/GithubApis.js
+++ src/scenes/GithubApis.js
@@ -9,13 +9,18 @@ import {
   Image,
 } from 'react-native';
 
+import {
+    Button,
+} from 'react-native-material-design';
+
 import * as GLOBAL from '../utils/Globals';
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: GLOBAL.COLOR.BACKGROUND,
         flex: 1,
-        justifyContent: 'flex-start',
+        justifyContent: 'center',
+        alignItems: 'center',
         paddingTop: 16,
     },
     loading: {
@@ -24,7 +29,22 @@ const styles = StyleSheet.create({
     profile: {
         width: 200,
         height: 200,
+        marginBottom: 8,
     },
+    name: {
+        fontSize: 20,
+        fontWeight: '500',
+        color: GLOBAL.COLOR.PRIMARY_TEXT,
+    },
+    handle: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: GLOBAL.COLOR.SECONDARY_TEXT,
+    },
+    follower_container: {
+        flexDirection: 'row',
+        marginTop: 16,
+    }
 });
 
 class GithubApis extends Component {
@@ -50,6 +70,7 @@ class GithubApis extends Component {
         const REQUEST_URL = this.github_url + this.github_username;
         global.LOG('REQUEST_URL',REQUEST_URL);
 
+        // TODO do error handling
         fetch(REQUEST_URL)
             .then((response) => response.json())
             .then((responseData) => {
@@ -60,17 +81,52 @@ class GithubApis extends Component {
             })
             .done();
     }
-
+    // {
+    //   "login": "jicksonp",
+    //   "id": 16488048,
+    //   "avatar_url": "https://avatars.githubusercontent.com/u/16488048?v=3",
+    //   "gravatar_id": "",
+    //   "url": "https://api.github.com/users/jicksonp",
+    //   "html_url": "https://github.com/jicksonp",
+    //   "followers_url": "https://api.github.com/users/jicksonp/followers",
+    //   "following_url": "https://api.github.com/users/jicksonp/following{/other_user}",
+    //   "gists_url": "https://api.github.com/users/jicksonp/gists{/gist_id}",
+    //   "starred_url": "https://api.github.com/users/jicksonp/starred{/owner}{/repo}",
+    //   "subscriptions_url": "https://api.github.com/users/jicksonp/subscriptions",
+    //   "organizations_url": "https://api.github.com/users/jicksonp/orgs",
+    //   "repos_url": "https://api.github.com/users/jicksonp/repos",
+    //   "events_url": "https://api.github.com/users/jicksonp/events{/privacy}",
+    //   "received_events_url": "https://api.github.com/users/jicksonp/received_events",
+    //   "type": "User",
+    //   "site_admin": false,
+    //   "name": "Jickson P",
+    //   "company": "Peppertap",
+    //   "blog": null,
+    //   "location": "Gurgaon,India",
+    //   "email": "jicksonstephen@gmail.com",
+    //   "hireable": null,
+    //   "bio": null,
+    //   "public_repos": 4,
+    //   "public_gists": 0,
+    //   "followers": 1,
+    //   "following": 6,
+    //   "created_at": "2015-12-30T10:45:35Z",
+    //   "updated_at": "2016-05-05T09:31:27Z"
+    // }
     render() {
         if (this.state.isLoaded) {
             return (
                 <View style={styles.container}>
-                    <Text>Data found</Text>
-                    <Text>{this.state.user.login}</Text>
                     <Image
                         source={{ uri: this.state.user.avatar_url }}
                         style={styles.profile}
                     />
+                    <Text style={styles.name}>{this.state.user.name}</Text>
+                    <Text style={styles.handle}>@{this.state.user.login}</Text>
+                    <View style={styles.follower_container}>
+                        <Button value="Followers" raised={true} onPress={()=> console.log("I pressed a flat button")} />
+                        <Button value="Following" raised={true} onPress={()=> console.log("I pressed a raised button")} />
+                    </View>
                 </View>
             );
         } else {
