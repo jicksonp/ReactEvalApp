576ccd58897ed9ad5157abc46f1ea4bfe6fc178e
Jickson P <jickson.p@peppertap.com>
2016-05-23 12:42:22 +0530

    integrated maps

--- android/app/src/main/java/com/reactevalapp/MainActivity.java
+++ android/app/src/main/java/com/reactevalapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.reactevalapp;
 
+import com.airbnb.android.react.maps.MapsPackage;
+import com.eguma.barcodescanner.BarcodeScanner;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-
-import com.eguma.barcodescanner.BarcodeScanner;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,7 +37,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new BarcodeScanner()
+            new BarcodeScanner(),
+            new MapsPackage()
         );
     }
 }
--- src/scenes/MapScreen.js
+++ src/scenes/MapScreen.js
@@ -8,125 +8,48 @@ import {
   StyleSheet,
 } from 'react-native';
 
-import {
-    Card,
-} from 'react-native-material-design';
 
 import * as GLOBAL from '../utils/Globals';
-import NetworkImageView from '../components/NetworkImageView';
+
+import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: GLOBAL.COLOR.BACKGROUND,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: 16,
-    },
-    card_container: {
-        padding: 16,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    loading: {
-        justifyContent: 'center',
-    },
-    profile: {
-        width: 200,
-        height: 200,
-        marginBottom: 8,
-    },
-    name: {
-        fontSize: 20,
-        fontWeight: '500',
-        color: GLOBAL.COLOR.PRIMARY_TEXT,
-    },
-    handle: {
-        fontSize: 14,
-        fontWeight: '300',
-        color: GLOBAL.COLOR.SECONDARY_TEXT,
-    },
-    email: {
-        fontSize: 16,
-        fontWeight: '400',
-        color: GLOBAL.COLOR.PRIMARY_TEXT,
-    },
-    follower_container: {
-        flexDirection: 'row',
-        marginTop: 16,
-    }
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 });
 
 class MapScreen extends Component {
 
     constructor(props, context) {
         super(props, context);
-        this.github_username = 'mojombo';
-        this.github_url = 'https://api.github.com/users/';
-
-        this.state = {
-            user: null,
-            isLoaded: null,
-        };
     }
 
-
-    componentDidMount() {
-        // TODO Move this to container later
-        //this.fetchUserData();
-    }
-
-    fetchUserData() {
-        const REQUEST_URL = this.github_url + this.github_username;
-        global.LOG('REQUEST_URL',REQUEST_URL);
-
-        // TODO do error handling
-        fetch(REQUEST_URL)
-            .then((response) => response.json())
-            .then((responseData) => {
-                this.setState({
-                    user: responseData,
-                    isLoaded: true,
-                })
-            })
-            .done();
-    }
-    // {
-    //   "login": "jicksonp",
-    //   "id": 16488048,
-    //   "avatar_url": "https://avatars.githubusercontent.com/u/16488048?v=3",
-    //   "gravatar_id": "",
-    //   "url": "https://api.github.com/users/jicksonp",
-    //   "html_url": "https://github.com/jicksonp",
-    //   "followers_url": "https://api.github.com/users/jicksonp/followers",
-    //   "following_url": "https://api.github.com/users/jicksonp/following{/other_user}",
-    //   "gists_url": "https://api.github.com/users/jicksonp/gists{/gist_id}",
-    //   "starred_url": "https://api.github.com/users/jicksonp/starred{/owner}{/repo}",
-    //   "subscriptions_url": "https://api.github.com/users/jicksonp/subscriptions",
-    //   "organizations_url": "https://api.github.com/users/jicksonp/orgs",
-    //   "repos_url": "https://api.github.com/users/jicksonp/repos",
-    //   "events_url": "https://api.github.com/users/jicksonp/events{/privacy}",
-    //   "received_events_url": "https://api.github.com/users/jicksonp/received_events",
-    //   "type": "User",
-    //   "site_admin": false,
-    //   "name": "Jickson P",
-    //   "company": "Peppertap",
-    //   "blog": null,
-    //   "location": "Gurgaon,India",
-    //   "email": "jicksonstephen@gmail.com",
-    //   "hireable": null,
-    //   "bio": null,
-    //   "public_repos": 4,
-    //   "public_gists": 0,
-    //   "followers": 1,
-    //   "following": 6,
-    //   "created_at": "2015-12-30T10:45:35Z",
-    //   "updated_at": "2016-05-05T09:31:27Z"
-    // }
     render() {
         return (
-            <View><Text>Hello world</Text></View>
-        )
+          <MapView
+            style={ styles.map }
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        );
     }
 }
 
